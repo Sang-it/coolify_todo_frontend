c50707e9b7ef874078a0276cc083a0d5f54915c6
Sangit Manandhar <rux12@icloud.com>
2025-11-14 10:07:34 -0500

    chore: change root

--- src/const.js
+++
@@ -1,3 +0,0 @@
-import process from "node:process";
-
-export const API_BASE = process.env.API_BASE;
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
-  plugins: [preact()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+    plugins: [preact()],
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true
+    },
+    root: "./src"
 });
